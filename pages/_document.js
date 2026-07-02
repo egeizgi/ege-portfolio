92aa56ce1a20aeca3b230e42c829f60b38cb7512
Ege Izgi <egeizgi10@gmail.com>
2026-07-02 14:22:13 +0300

    Add AdSense site verification meta tag to homepage

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="google-adsense-account" content="ca-pub-6061278849571695" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
